bafa6629ffbe96af24cefac8b4b7b64f550a59aa
sakjanonkk <sakjanon.k@kkumail.com>
2025-09-25 09:27:55 +0700

    .

--- pkg/domain/staff.go
+++ pkg/domain/staff.go
@@ -6,13 +6,13 @@ type StaffService interface {
 	CreateStaff(models.Staff) (staff models.Staff, err error)
 	GetStaffs(models.Pagination) (staffs []models.Staff, pagination models.Pagination, err error)
 	GetStaffByID(models.Staff) (staff models.Staff, err error)
-	UpdateStaff(models.Staff) (staff models.Staff, err error) // ✅ update
-	DeleteStaff(models.Staff) error                           // ✅ delete
+	UpdateStaff(models.Staff) (staff models.Staff, err error)
+	DeleteStaff(models.Staff) error
 }
 type StaffReposity interface {
 	CreateStaff(models.Staff) (staff models.Staff, err error)
 	GetStaffs(models.Pagination) (staffs []models.Staff, pagination models.Pagination, err error)
 	GetStaffByID(models.Staff) (staff models.Staff, err error)
-	UpdateStaff(models.Staff) (staff models.Staff, err error) // ✅ update
-	DeleteStaff(models.Staff) error                           // ✅ delete
+	UpdateStaff(models.Staff) (staff models.Staff, err error)
+	DeleteStaff(models.Staff) error
 }
