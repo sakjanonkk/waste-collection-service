6aaff51b6fd540ecb7c150fd0073636ddddbd402
sakjanonkk <sakjanon.k@kkumail.com>
2025-12-03 17:20:34 +0700

    feat : fix core error for https

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -20,7 +20,6 @@ import (
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host localhost:8080
 // @BasePath /api/v1
 // @schemes http https
 // @securityDefinitions.apikey ApiKeyAuth
